4a734f9862ba7564ae6e584454266bebf8d3365c
deathbybandaid <github@deathbybandaid.net>
2020-11-11 12:35:03 -0500

    EPG and VLC and Tuner Improvements

--- fHDHR/origin/origin_service.py
+++ fHDHR/origin/origin_service.py
@@ -2,10 +2,8 @@
 
 class OriginService():
 
-    def __init__(self, settings, logger, web):
-        self.config = settings
-        self.logger = logger
-        self.web = web
+    def __init__(self, fhdhr):
+        self.fhdhr = fhdhr
 
     def get_status_dict(self):
         ret_status_dict = {}
